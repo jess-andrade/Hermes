0102afc13356f1d04fdcc345191bcdaa1b0771d3
jess <jessiewq1@gmail.com>
2023-07-03 21:19:46 +0200

    feat: button Trend Topics

--- src/components/TrendButton.tsx
+++ src/components/TrendButton.tsx
@@ -1,10 +1,18 @@
 import Image from "next/image";
 import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 
+import { useRouter } from 'next/router'
+
 export default function () {
+  const router = useRouter()
+
+  const handleClick = () => {
+    router.push('/')
+  }
   return (
     <div
       className={`p-4 flex items-center flex-nowrap cursor-pointer space-x-8 w-3/4 h-16 rounded-lg border-b-4 border-indigo-950 bg-[#1d267d]`}
+      onClick={handleClick}
     >
       <div className={`flex flex-row gap-x-10 m-4`}>
         {/* name and description */}
