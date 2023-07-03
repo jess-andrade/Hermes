8c442b6869b550f1ac04419a5a76310e1d7bc578
João Paulo Rios <joao.paulo.rios@hotmail.com>
2023-07-03 12:46:41 -0300

    feat: searchBar almost done

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,17 +1,18 @@
-import Fields from './Fields';
+import Fields from "./Fields";
 
-import Image from 'next/image'
-import bad from '../../public/bad.png'
-import hermes from '../../public/hermes.png'
+import Image from "next/image";
+import bad from "../../public/bad.png";
+import hermes from "../../public/hermes.png";
+import SearchBar from "./SearchBar";
 
 export default function Search() {
   return (
     <>
       <div className={`flex flex-col w-full rounded-xl`}>
         <div className={` flex-col flex w-full justify-center`}>
-          <Fields />
+          <SearchBar />
         </div>
       </div>
     </>
-  )
+  );
 }
