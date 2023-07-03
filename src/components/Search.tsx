305fe7b439a77190c8bd15aaf6c4b4bf57b7a576
jess <jessiewq1@gmail.com>
2023-07-03 18:19:30 +0200

    feat: Trend Button

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,15 +1,15 @@
-import Fields from './Fields';
+import Fields from './TrendButton';
 
 import Image from 'next/image'
-import bad from '../../public/bad.png'
 import hermes from '../../public/hermes.png'
+import TrendButton from './TrendButton';
 
 export default function Search() {
   return (
     <>
       <div className={`flex flex-col w-full rounded-xl`}>
-        <div className={` flex-col flex w-full justify-center`}>
-          <Fields />
+        <div className={` flex-col flex gap-8 w-full justify-center items-center`}>
+          <TrendButton />
         </div>
       </div>
     </>
